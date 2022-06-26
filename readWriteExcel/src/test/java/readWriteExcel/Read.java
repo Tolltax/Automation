6a5db1af27c65d3fb50f51395fa7a00ae2f8e484
Rakshith HR <rakshith@cstech.in>
2022-06-26 23:36:35 +0530

    1st

---
+++ readWriteExcel/src/test/java/readWriteExcel/Read.java
@@ -0,0 +1,25 @@
+package readWriteExcel;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Read {
+
+	
+public static void main(String [] args) throws IOException
+	
+	{
+		System.out.println("TEST");
+
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ readWriteExcel/src/test/java/readWriteExcel/write.java
@@ -0,0 +1,5 @@
+package readWriteExcel;
+
+public class write {
+
+}
